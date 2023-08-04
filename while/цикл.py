10af71ca919e25cf89b2f21513a59b6b7d657fee
enty-303 <140957086+enty-303@users.noreply.github.com>
2023-08-05 01:18:01 +0200

    Добавил первую версию регистратора игроков. Думаю прокачать.

---
+++ while/цикл.py
@@ -0,0 +1,6 @@
+players = 0
+answer = 'да'
+while answer == 'да':
+    players = players + 1
+    print('Игроков на сервере в майнкрафте: ' +str(players))
+    answer = input('Разрешить игроку зайти на сервер? (да/нет) ')
