653e515dd44ecd5b4aea590e4b010935c3482193
enty-303 <140957086+enty-303@users.noreply.github.com>
2023-11-12 16:46:44 +0200

    Сделал тест про космодесантников из вархаммера 40000

---
+++ тесты/astartres.py
@@ -0,0 +1,24 @@
+def check_gues(guess, answer):
+    global score
+    still_guessing = True
+    attempt = 0
+    while still_guessing and attempt < 3:
+        if guess.lower() == answer.lower():
+            print('The answer is correct.')
+            score = score + 1
+            still_guessing = False
+        else:
+            if attempt < 2:
+                guess = input('The answer is incorrect. Try again.')
+            attempt = attempt + 1
+    if attempt == 3:
+        print('right answer: ' + answer)
+score = 0
+print('"Test "Adeptus Astartres"')
+guess1 = input('What weapons do tactical guards have?')
+check_gues(guess1, 'bbolterolter and combat blade')
+guess2 = input('In which company are the terminators?')
+check_gues(guess2, 'in the first company')
+guess3 = input('Who produces the space marine armor?')
+check_gues(guess3, 'Adeptus Mechanicus')
+print('You scored points: ' + str(score))
